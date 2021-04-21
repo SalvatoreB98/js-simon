9d3afe68b7510db0015b89c224dcbdc6fe25a988
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-21 18:10:39 +0200

    Update quanti e quali sono stati individuati. OK

--- myScript.js
+++ myScript.js
@@ -1,24 +1,38 @@
 var numeriRandom = [];
 var N = 5;
 var delayInSec = 3;
+numeriIndovinatiArray = [];
 
 
 for (var i = 0; i < N; i++) {
-    numeriRandom.push(randomNumber(30,50));
+    numeriRandom.push(randomNumber(30, 50));
 }
 alert(numeriRandom.join("  -  "));
 console.log(numeriRandom);
 
 setTimeout(function () {
-    var numeriIndovinati=0;
+    var numeriIndovinati = 0;
     for (var i = 0; i < N; i++) {
-        var numeroUtente= parseInt(prompt("Testa le tue capacità cognitive. Quanti numeri ricordi? Inserisci i numeri uno alla volta"));
-        if(numeriRandom.indexOf(numeroUtente)>-1){
-            numeriIndovinati ++;
+        var numeroUtente = 0;
+
+        do {
+            numeroUtente = parseInt(prompt("Testa le tue capacità cognitive. Quanti numeri ricordi? Inserisci i numeri uno alla volta"));
+            if (isNaN(numeroUtente ) ==  true) {
+                alert("Attenzione inserire un numero!");
+            }
+        } while (isNaN(numeroUtente) == true);
+
+        if (numeriRandom.indexOf(numeroUtente) > -1) {
+            numeriIndovinati++;
+            numeriIndovinatiArray.push(numeroUtente);
         }
     }
-    alert("HAI INDOVINATO " + numeriIndovinati + " numeri");
-}, delayInSec*1000);
+    alert("Hai indovinato i seguenti numeri: " + numeriIndovinatiArray.join("  -  "))
+    alert("Il tuo punteggio e' " + numeriIndovinati + "/" + N);
+    if (numeriIndovinati == N) {
+        alert("COMPLIMENTI HAI VINTO!");
+    }
+}, delayInSec * 1000);
 
 
 
@@ -39,7 +53,7 @@ setTimeout(function () {
 
 
 // FUNZIONI
-function randomNumber(min,max) {
-    return  Math.floor(Math.random() * (max - min)) + min;
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
