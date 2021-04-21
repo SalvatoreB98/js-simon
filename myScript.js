d486778275c39bb22eaa84800243486a605f5af9
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-21 17:48:04 +0200

    Esercizio parte input completata

--- myScript.js
+++ myScript.js
@@ -1,5 +1,45 @@
 var numeriRandom = [];
-var N = 5
-alert("ATTENZIONE");
+var N = 5;
+var delayInSec = 3;
 
+
+for (var i = 0; i < N; i++) {
+    numeriRandom.push(randomNumber(30,50));
+}
+alert(numeriRandom.join("  -  "));
+console.log(numeriRandom);
+
+setTimeout(function () {
+    for (var i = 0; i < N; i++) {
+        var numeroUtente= prompt("Testa le tue capacità cognitive. Quanti numeri ricordi? Inserisci i numeri uno alla volta");
+        var numeriIndovinati=0;
+        if(numeriRandom.indexOf()){
+            numeriIndovinati ++;
+        }
+    }
+    prompt("HAI INDOVINATO " + numeriIndovinati + " numeri");
+}, delayInSec*1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNZIONI
+function randomNumber(min,max) {
+    return  Math.floor(Math.random() * (max - min)) + min;
+}
 
