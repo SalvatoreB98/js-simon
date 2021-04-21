2fb8722a94986975125f8cfbc36f7d11cbf4b20e
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-21 18:45:39 +0200

    BUG FIX

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ numeriIndovinatiArray = [];
 for (var i = 0; i < N; i++) {
     numeriRandom.push(randomNumber(30, 50));
 }
-alert(numeriRandom.join("  -  "));
+alert("Testa le tue capacità cognitive! Quanti di questi numeri ricordi dopo " + delayInSec + 1 + " secondi? \n" + numeriRandom.join("  -  "));
 console.log(numeriRandom);
 
 setTimeout(function () {
@@ -16,41 +16,30 @@ setTimeout(function () {
         var numeroUtente = 0;
 
         do {
-            numeroUtente = parseInt(prompt("Testa le tue capacità cognitive. Quanti numeri ricordi? Inserisci i numeri uno alla volta"));
-            if (isNaN(numeroUtente ) ==  true) {
+            numeroUtente = parseInt(prompt("Inserisci i numeri che ricordi uno alla volta"));
+            if (isNaN(numeroUtente) == true) {
                 alert("Attenzione inserire un numero!");
             }
         } while (isNaN(numeroUtente) == true);
-
-        if (numeriRandom.indexOf(numeroUtente) > -1) {
+        // CONTROLLA SE IL NUMERO E' STATO INDOVINATO (CONTROLLA NELL'ARRAY DEI NUMERI RANDOM) E POI SI ASSICURA CHE QUEL NUMERO NON SIA GIA' STATO INDOVINATO  
+        if (numeriRandom.indexOf(numeroUtente) > -1 && numeriIndovinatiArray.indexOf(numeroUtente) == -1 ) {
             numeriIndovinati++;
             numeriIndovinatiArray.push(numeroUtente);
+            console.log(numeriRandom);
+            console.log(numeriIndovinatiArray);
         }
     }
-    alert("Hai indovinato i seguenti numeri: " + numeriIndovinatiArray.join("  -  "))
+    if (numeriIndovinati > 0) {
+        alert("Hai indovinato i seguenti numeri: \n" + numeriIndovinatiArray.join("  -  "))
+    }
+    else{
+        alert("Non hai ricordato nessun numero, ritenta!");
+    }
     alert("Il tuo punteggio e' " + numeriIndovinati + "/" + N);
     if (numeriIndovinati == N) {
         alert("COMPLIMENTI HAI VINTO!");
     }
 }, delayInSec * 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // FUNZIONI
 function randomNumber(min, max) {
